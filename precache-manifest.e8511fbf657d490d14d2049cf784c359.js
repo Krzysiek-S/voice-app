8642bce83fce1fc44b95308f23179727aa8609fc
react <reactm4@gmail.com>
2020-11-04 14:11:00 +0100

    Updates

---
+++ precache-manifest.e8511fbf657d490d14d2049cf784c359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d083c1963a97aecf645ff06d9463a4c",
+    "url": "/voice-app/index.html"
+  },
+  {
+    "revision": "5e920e5668eb36113679",
+    "url": "/voice-app/static/css/main.78ee2faf.chunk.css"
+  },
+  {
+    "revision": "f76c78ca6c87903972f5",
+    "url": "/voice-app/static/js/2.b7b5f711.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/voice-app/static/js/2.b7b5f711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e920e5668eb36113679",
+    "url": "/voice-app/static/js/main.79e76ac6.chunk.js"
+  },
+  {
+    "revision": "0827d5c4bcab0198fba8",
+    "url": "/voice-app/static/js/runtime-main.1085b746.js"
+  }
+]);
